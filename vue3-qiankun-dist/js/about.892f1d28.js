38d45da981ff1548095c5bf61195e10fcc07fe13
lqs5858@163.com <LQS5858@163.COM>
2021-04-10 18:44:08 +0800

    delete

--- vue3-qiankun-dist/js/about.892f1d28.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp_vue3-qiankun"]=window["webpackJsonp_vue3-qiankun"]||[]).push([["about"],{f820:function(n,u,a){"use strict";a.r(u);var e=a("7a23"),t={class:"about"},c=Object(e["f"])("h1",null,"This is an about page",-1);function o(n,u){return Object(e["o"])(),Object(e["c"])("div",t,[c])}const i={};i.render=o;u["default"]=i}}]);
-//# sourceMappingURL=about.892f1d28.js.map
